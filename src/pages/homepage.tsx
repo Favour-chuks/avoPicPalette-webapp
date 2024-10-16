d97d37a297495f64f847455bfc28ebdfa7c7ad82
Favour chuks <84434963+Favour-chuks@users.noreply.github.com>
2024-10-16 17:07:37 +0100

    Update homepage.tsx

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,11 @@
+import 'react' fron react;
+
+export default function HomePage(){
+    return(
+    <>
+        <div>
+            this is the heading of the homepage
+        </div>
+    </>
+    )
+}
